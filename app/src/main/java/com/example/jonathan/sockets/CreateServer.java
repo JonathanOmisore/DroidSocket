919891b881ff72d77d3f61e77fa5e072df9bb2fe
comprogger <xiicubed@gmail.com>
2016-11-22 21:54:35 -0500

    Create

---
+++ app/src/main/java/com/example/jonathan/sockets/CreateServer.java
@@ -0,0 +1,41 @@
+package com.example.jonathan.sockets;
+import java.net.*;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class CreateServer extends Thread {
+    ServerSocket serverSocket;
+    Socket clientSocket;
+    TextView mText;
+    Button mbutton;
+
+    public void run(){
+        try{
+
+    }
+        catch(Exception e){
+        }
+    }
+
+    CreateServer(int port, TextView view, Button button){
+        mbutton = button;
+        mText = view;
+        try{
+            mbutton.setVisibility(View.INVISIBLE);
+            serverSocket = new ServerSocket(port);
+
+            clientSocket = serverSocket.accept();
+            mText.setText("Received connection from " + clientSocket.getRemoteSocketAddress());
+        }
+        catch(Exception e){
+            Log.e("main_activity",e.toString());
+
+
+        }
+
+
+    }
+}
